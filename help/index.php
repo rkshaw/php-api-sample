ee4a3ccc1752423976cc24c6788c40edf2d76b4a
Rahul Kumar Shaw <rahulshaw480@gmail.com>
2025-07-27 18:37:04 +0530

    Removed additional library files

--- help/index.php
+++ help/index.php
@@ -4,24 +4,24 @@
   <head>
     <meta charset="UTF-8">
     <title>Swagger UI</title>
-    <link rel="stylesheet" type="text/css" href="../swagger-ui/dist/swagger-ui.css" />
-    <link rel="stylesheet" type="text/css" href="../swagger-ui/dist/index.css" />
-    <link rel="icon" type="image/png" href="../swagger-ui/dist/favicon-32x32.png" sizes="32x32" />
-    <link rel="icon" type="image/png" href="../swagger-ui/dist/favicon-16x16.png" sizes="16x16" />
+    <link rel="stylesheet" type="text/css" href="../swagger-ui/swagger-ui.css" />
+    <link rel="stylesheet" type="text/css" href="../swagger-ui/index.css" />
+    <link rel="icon" type="image/png" href="../swagger-ui/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="../swagger-ui/favicon-16x16.png" sizes="16x16" />
   </head>
 
   <body>
     <div id="swagger-ui"></div>
-    <script src="../swagger-ui/dist/swagger-ui-bundle.js" charset="UTF-8"> </script>
-    <script src="../swagger-ui/dist/swagger-ui-standalone-preset.js" charset="UTF-8"> </script>
-    <!-- <script src="../swagger-ui/dist/swagger-initializer.js" charset="UTF-8"> </script> -->
+    <script src="../swagger-ui/swagger-ui-bundle.js" charset="UTF-8"> </script>
+    <script src="../swagger-ui/swagger-ui-standalone-preset.js" charset="UTF-8"> </script>
+    <!-- <script src="../swagger-ui/swagger-initializer.js" charset="UTF-8"> </script> -->
     <script>
     window.onload = function() {
       //<editor-fold desc="Changeable Configuration Block">
 
       // the following lines will be replaced by docker/configurator, when it runs in a docker-container
       window.ui = SwaggerUIBundle({
-        url: "http://localhost/php-api/help/api.php",
+        url: "api.php",
         dom_id: '#swagger-ui',
         deepLinking: true,
         presets: [
